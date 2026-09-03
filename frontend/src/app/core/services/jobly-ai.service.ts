6bd7a15bcb4bde2ff6ea53dc37bebe5f70146d22
Ezequiel Salazar <ezequielsc017@gmail.com>
2026-09-03 14:43:05 -0600

    fix: correct render url

--- frontend/src/app/core/services/jobly-ai.service.ts
+++ frontend/src/app/core/services/jobly-ai.service.ts
@@ -102,7 +102,7 @@ export interface CvGeneratorAnswerResponse {
   providedIn: 'root'
 })
 export class JoblyAiService {
- private readonly aiApiUrl = 'https://jobly-ai-api.onrender.com';
+  private readonly aiApiUrl = 'https://jobly-ai-api.onrender.com/'; 
   
 
   private readonly jobsApiUrl = '/api/jobs';
